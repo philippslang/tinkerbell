9e42688c79cd76914744ceb1a8582bb1d0f12b22
Philipp Lang <PLang@slb.com>
2017-07-03 13:04:13 +0100

    bspline fitting

--- approximate.py
+++ approximate.py
@@ -25,29 +25,36 @@ def least_square(points, h, p=3):
         raise ValueError('Inputs dont honor len(points)-1 > h-1 >= p >= 1')
     pass
 
+if 0:
+    pts_fname = 'data_demo/points_01.json'
+    pts = pt.read_points(pts_fname)
+    pts_coords = [np.array(pt.point_coordinates(pts, i)) for i in range(2)]
 
-pts_fname = 'data_demo/points_00.json'
-pts_fname = 'data_demo/points_01.json'
-pts = pt.read_points(pts_fname)
+    # approcimating spline
+    t = [10, 25, 40]
+    t = [25]
+    t = [np.median(pts_coords[0])]
+else:
+    pts_fname = 'data_demo/points_02.json'
+    pts = pt.read_points(pts_fname)
+    pts_coords = [np.array(pt.point_coordinates(pts, i)) for i in range(2)]
 
-pts_coords = [np.array(pt.point_coordinates(pts, i)) for i in range(2)]
+    # approcimating spline
+    t = np.linspace(10, 30, 7)
 
-# approcimating spline
-t = [10, 25, 40]
 k = 3
 t = np.r_[(pts_coords[0][0],)*(k+1), t, (pts_coords[0][-1],)*(k+1)]
 spl = make_lsq_spline(*pts_coords, t, k)
+
 print(spl.t)
 print(spl.c)
 print(spl.k)
-spl = BSpline(spl.t, spl.c, spl.k)
 
 fig = plt.figure() 
 ax = fig.add_subplot(111)
 pt.plot_points(ax, pts)
 ax.plot(pts_coords[0], spl(pts_coords[0]), 'g-', lw=3, label='LSQ spline')
 plt.show()
-
 
 
         
--- factory.py
+++ factory.py
@@ -18,15 +18,22 @@ def exponential_decline(y_i, d, x):
 
 y_i = 50
 d = 0.1
-xdata = np.linspace(0, 50,)
+xdata = np.linspace(0, 50)
 ydata = exponential_decline(y_i, d, xdata) + 10.0
-ydata = ydata * np.random.normal(1.0, 0.1, ydata.shape)
-pts = [pt.Point(x, y) for x, y in zip(xdata, ydata)]
-pt.write_points('data_demo/points_01.json', pts)
+ydata_noise = ydata * np.random.normal(1.0, 0.1, ydata.shape)
+pts_exp_cont = [pt.Point(x, y) for x, y in zip(xdata, ydata_noise)]
+pt.write_points('data_demo/points_01.json', pts_exp_cont)
+
+xchangeidx = 20
+ydata_noise = ydata * np.random.normal(1.0, 0.1, ydata.shape)
+ydata_noise[xchangeidx:] = ydata_noise[xchangeidx:] + 10.0
+pts_exp_discont_01 = [pt.Point(x, y) for x, y in zip(xdata, ydata_noise)]
+pt.write_points('data_demo/points_02.json', pts_exp_discont_01)
 
 fig = plt.figure() 
 ax = fig.add_subplot(111)
-pt.plot_points(ax, pts)
+pt.plot_points(ax, pts_exp_cont)
+pt.plot_points(ax, pts_exp_discont_01)
 plt.show()
 
 
