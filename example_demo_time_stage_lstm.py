4182731719fa43344519dcc20396f110b89612cc
Philipp Lang <PLang@slb.com>
2017-07-17 14:22:23 +0100

    demo figure changes

--- example_demo_time_stage_lstm.py
+++ example_demo_time_stage_lstm.py
@@ -6,6 +6,10 @@ import example_generate_time_stage_training_set as exgen
 import example_regress_on_time_stage_training_set as exregr
 import example_load_time_stage_lstm_predict as expred
 
+
+FNAME_IMG = 'img/time_stage_lstm.png'
+
+
 def xy_from_npy(fname):
     data = np.load(fname)
     return (data[0], data[1])
@@ -17,22 +21,27 @@ lims = (xlim, ylim)
 fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, sharex='col', sharey='row')
 
 training_xy = xy_from_npy(exregr.FNAME_TRAIN)
-tbapl.render(ax1, [training_xy], lim=lims)
+inputxdisc_xy = ((exgen.XDISC), (0))
+tbapl.render(ax1, [training_xy, inputxdisc_xy], styles=['p', 'ix'], lim=lims)
 ax1.set_title('Training')
 
 inputy0_xy = ((0), (expred.Y0))
 inputxdisc_xy = ((expred.XDISC), (0))
-tbapl.render(ax2, [training_xy, inputy0_xy, inputxdisc_xy], styles=['p', 'iy', 'ix'], lim=lims)
-ax2.set_title('Input')
+#tbapl.render(ax2, [training_xy, inputy0_xy, inputxdisc_xy], styles=['p', 'iy', 'ix'], lim=lims)
+tbapl.render(ax2, [inputy0_xy, inputxdisc_xy], styles=['iy', 'ix'], lim=lims)
+ax2.set_title('Query')
 
 prediction_xy = xy_from_npy(expred.FNAME_PRED)
-tbapl.render(ax3, [training_xy, inputy0_xy, inputxdisc_xy, prediction_xy], styles=['p', 'iy', 'ix', 'l'], lim=lims)
+#tbapl.render(ax3, [training_xy, inputy0_xy, inputxdisc_xy, prediction_xy], styles=['p', 'iy', 'ix', 'l'], lim=lims)
+tbapl.render(ax3, [inputy0_xy, inputxdisc_xy, prediction_xy], styles=['iy', 'ix', 'l'], lim=lims)
 ax3.set_title('Prediction')
 
 blind_xy = xy_from_npy(expred.FNAME_BLIND)
-tbapl.render(ax4, [blind_xy, prediction_xy], styles=['p', 'l'], lim=lims)
+tbapl.render(ax4, [prediction_xy, blind_xy], styles=['l', 'p'], lim=lims)
 ax4.set_title('Prediction vs. reference')
 
+fig.set_size_inches(14, 9)
+plt.savefig(FNAME_IMG, dpi=300)
 plt.show()
 
 
