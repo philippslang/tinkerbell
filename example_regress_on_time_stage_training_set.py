df4262ebf4d198cddc9522c20a254fb5c00947c1
plang85 <philipp.s.lang@gmail.com>
2017-07-18 21:33:55 +0100

    normalizer extracted

--- example_regress_on_time_stage_training_set.py
+++ example_regress_on_time_stage_training_set.py
@@ -25,59 +25,29 @@ import tinkerbell.app.model as tbamd
 import pickle
 
 FNAME = 'data_demo/model_lstm_stages_exp.h5'
-FNAME_INORM = 'data_demo/inorm'
-FNAME_ONORM = 'data_demo/onorm'
+FNAME_NORM = 'data_demo/norm'
 FNAME_TRAIN = 'data_demo/time_stage_lstm_train.npy'
 
 
 def do_the_thing(fit=True, num_epochs=1500, num_neurons=3):  
     series = pd.read_csv('data_demo/shale_time_stage_exp.csv')
-    #print('SERIES')
     #print(series.head())
 
     y = series['y'].values
     stage = series['stage'].values
     features = tbamd.Features(y, stage)
-
-    # we predict from previous y value, so features = labels with
-    # a shift. first a 1D array
-    
-    #print('Y')
-    #print(y, y.shape)
-
-    #stage =  series['stage'].values
-    #print('STAGE')
-    #print(stage, stage.shape)
-
-    # stationary features (delta y), essentially start at time = 1, 
-    # so one short of all labels
-    #ydeltaoutput = np.diff(y)
-    #print('YDELTAOUTPUT')
-    #print(ydeltaoutput, ydeltaoutput.shape)
-
-    #stagedeltainput = np.diff(stage)
-    #print('STAGEDELTA')
-    #print(stagedeltainput, stagedeltainput.shape)
+    targets = tbamd.Targets(y)
 
     # bring the labels to shape
     yinput = features.production[:-1]
-    #print('YINPUT')
-    #print(yinput, yinput.shape)
 
     # normalize both
-    input = np.transpose(np.array([yinput, features.dstage_dstep])) #yinput.reshape(-1, 1)
-    normalizer_input = skprep.MinMaxScaler(feature_range=(-1, 1))
-    input_normalized = normalizer_input.fit_transform(input)
-    pickle.dump(normalizer_input, open(FNAME_INORM, "wb"))
-    #print('INPUT NORM')
-    #print(input_normalized, input_normalized.shape)
-    ydeltaoutput = features.dproduction_dt.reshape(-1, 1)
-    normalizer_ydeltaoutput = skprep.MinMaxScaler(feature_range=(-1, 1))    
-    ydelta_normalized = normalizer_ydeltaoutput.fit_transform(ydeltaoutput)
-    pickle.dump(normalizer_ydeltaoutput, open(FNAME_ONORM, "wb"))    
-    #print('YDELTAOUTPUT NORM')
-    #print(ydelta_normalized, ydelta_normalized.shape)
-
+    input = features.matrix() 
+    ydeltaoutput = targets.matrix()
+    normalizer = tbamd.Normalizer.fit(input, ydeltaoutput) 
+    normalizer.save(FNAME_NORM)
+    input_normalized = normalizer.normalize_features(input)  
+    ydelta_normalized = normalizer.normalize_targets(ydeltaoutput)
 
     if fit:
         model = tbamd.lstm(input_normalized, ydelta_normalized, 1, num_epochs, num_neurons)
@@ -91,11 +61,11 @@ def do_the_thing(fit=True, num_epochs=1500, num_neurons=3):
         yprevious = yhat[-1]
         stagedeltacurrent = features.dstage_dstep[i-1]
         yinput = np.array([[yprevious, stagedeltacurrent]])
-        yinput_normalized = normalizer_input.transform(yinput)
+        yinput_normalized = normalizer.normalize_features(yinput)
         yinput_normalized = yinput_normalized.reshape(yinput_normalized.shape[0], 1, 
         yinput_normalized.shape[1])
         ydelta_normalized = model.predict(yinput_normalized, batch_size=1)
-        ydeltaoutput = normalizer_ydeltaoutput.inverse_transform(ydelta_normalized)
+        ydeltaoutput = normalizer.denormalize_targets(ydelta_normalized)
         ydeltaoutput = ydeltaoutput[0, 0]
         yhat += [yprevious+ydeltaoutput]
 
@@ -105,4 +75,4 @@ def do_the_thing(fit=True, num_epochs=1500, num_neurons=3):
     tbapl.plot([(xplot, y), (xplot, yhat)], styles=['p', 'l'], labels=['ytrain', 'yhat'])
 
 if __name__ == '__main__':
-  do_the_thing()
+  do_the_thing(True, 99, 3)
--- tinkerbell/app/model.py
+++ tinkerbell/app/model.py
@@ -1,18 +1,84 @@
+import numpy as np
+import pickle
 import logging as log
 import keras.models as kem
 import keras.layers as kel
-import numpy as np
+import sklearn.preprocessing as skprep
+
+
+def makes_deep_copy(fct):
+    def ret_fct(*args, **kwargs):
+        print('Deep copy of potentially large array in \'{}\'()'.format(fct.__name__))
+        return fct(*args, **kwargs)
+    ret_fct.f = fct.__name__
+    ret_fct.__doc__ = fct.__doc__
+    ret_fct.__dict__.update(fct.__dict__)
+    return ret_fct
 
 
 class Features:
+    @makes_deep_copy
     def __init__(self, production, stage):
         self.production = np.copy(production)
         self.stage = np.copy(stage)
         self.eval_deltas()
 
     def eval_deltas(self):
-        self.dproduction_dt = np.diff(self.production)
         self.dstage_dstep = np.diff(self.stage)
+
+    @makes_deep_copy
+    def matrix(self):
+        # here we must account for that we lost the top row
+        # when taking the delta from the production stage
+        # this is coupled with matrix() in Targets in a sense
+        # through the diff in the targets (we predict gradients)
+        return np.transpose(np.array([self.production[:-1], self.dstage_dstep]))
+
+
+class Targets:
+    @makes_deep_copy
+    def __init__(self, production):
+        self.production = np.copy(production)
+        self.eval_deltas()
+
+    def eval_deltas(self):
+        self.dproduction_dt = np.diff(self.production)
+
+    @makes_deep_copy
+    def matrix(self):
+        return self.dproduction_dt.reshape(-1, 1)
+
+
+class Normalizer:
+    def __init__(self):
+        self.features = skprep.MinMaxScaler(feature_range=(-1, 1))
+        self.targets = skprep.MinMaxScaler(feature_range=(-1, 1))
+
+    def normalize_features(self, feature_matrix):
+        return self.features.transform(feature_matrix)
+
+    def denormalize_features(self, feature_matrix):
+        return self.features.inverse_transform(feature_matrix)
+
+    def normalize_targets(self, target_matrix):
+        return self.targets.transform(target_matrix)
+
+    def denormalize_targets(self, target_matrix):
+        return self.targets.inverse_transform(target_matrix)
+
+    @staticmethod
+    def fit(feature_matrix, targets_matrix):
+        normalizer = Normalizer()
+        normalizer.features.fit(feature_matrix)
+        normalizer.targets.fit(targets_matrix)
+        return normalizer
+
+    def save(self, fname):
+        pickle.dump(self, open(fname, "wb"))
+
+    @staticmethod
+    def load(fname):
+        return pickle.load(open(fname, "rb"))
 
 
 class ProgressBar:
@@ -21,11 +87,8 @@ class ProgressBar:
         self.length = 50
         self.decimals = 1
         self.num_iterations = num_iterations
-        self.leading_newline = True
 
     def __enter__(self):
-        if self.leading_newline:
-            print()
         self.update()
         return self
 
@@ -33,10 +96,14 @@ class ProgressBar:
         print()
 
     def update(self, iteration=0, history=None):
-        fraction = ("{0:." + str(self.decimals) + "f}").format(100 * (iteration/self.num_iterations)))
+        iteration = iteration + 1
+        fraction = ("{0:." + str(self.decimals) + "f}").format(100.0*iteration/self.num_iterations)
         num_filled = int(self.length * iteration // self.num_iterations)
         bar = self.fill * num_filled + '-' * (self.length - num_filled)
-        print('\rTraining |%s| %s%% complete, loss = %f.' % (bar, fraction, history.history['loss']), end='\r')
+        loss = 0.0
+        if history:
+            loss = history.history['loss'][-1]
+        print('\rTraining |%s| %s%% complete, loss = %f.' % (bar, fraction, loss), end='\r')
 
 
 def train(model, X, y, num_epochs, batch_size):
@@ -47,9 +114,10 @@ def train(model, X, y, num_epochs, batch_size):
             progress_bar.update(i, history)
 
 
-def lstm(features, labels, batch_size, num_epochs, num_neurons):
+@makes_deep_copy
+def lstm(features, targets, batch_size, num_epochs, num_neurons):
     log.info('LSTM model with {0:d} neurons'.format(num_neurons))
-    X, y = features, labels[:, 0]
+    X, y = features, targets[:, 0]
     X = X.reshape(X.shape[0], 1, X.shape[1])
     model = kem.Sequential()
     model.add(kel.LSTM(num_neurons, batch_input_shape=(batch_size, X.shape[1], X.shape[2]), stateful=True))
