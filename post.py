8f8b26e0f41bf4a13facc5c50a6f171118620518
plang85 <philipp.s.lang@gmail.com>
2017-08-21 07:42:24 +0100

    cosmetics

--- post.py
+++ post.py
@@ -3,12 +3,6 @@ Whole sequence lstm trained on multiple sequences, incl NA val
 """
 
 import tinkerbell.app.plot as tbapl
-import tinkerbell.domain.make as tbdmk
-import tinkerbell.domain.point as tbdpt
-import tinkerbell.app.make as tbamk
-import tinkerbell.app.model as tbamd
-import tinkerbell.app.rcparams as tbarc
-import pandas as pd
 import numpy as np
 import sklearn.preprocessing as preproc
 import pickle, sys, collections
@@ -23,39 +17,59 @@ def l():
 
 
 FNAME_MODEL = 'data_demo/model_post.h5'
-FNAME_NORM = 'data_demo/norm_post'
-XDISC_MIN = 20.0
-XDISC_MAX = 40.0
-XMAX = 70.0
-NUM_PTS = 50
-D = 0.1
+
+
+def calc_exponential_decline(p0, exp, time):
+    return p0 * np.exp(-exp * time)
+
+
+def calc_two_stage_decline(p0, exp_stage_zero, exp_stage_one, time_max, time_next_stage, time_min=0., 
+  production_jumpfactor=30.0, num=50, noise=0.1, noise_mean=1.):
+    time = np.linspace(time_min, time_max, num)
+    next_stage = np.where(time > time_next_stage)    
+    production = calc_exponential_decline(p0, exp_stage_zero, time) * np.random.normal(noise_mean, noise, time.shape) 
+    time_stage_one_relative = np.linspace(time_min, time_max-time_next_stage, len(time[next_stage]))
+    production[next_stage] = calc_exponential_decline(np.sqrt(production[next_stage][0]**2 * production_jumpfactor) * 
+      np.random.normal(noise_mean, noise*3), exp_stage_one, time_stage_one_relative) * np.random.normal(noise_mean, noise*2, time_stage_one_relative.shape)
+    stage = np.zeros_like(time)
+    stage[next_stage] = 1.0
+    production[production < 0.] = 0.
+    return time, production, stage
+
+
+def make_rnn(num_features, num_targets, num_timesteps, num_units):
+    model = kem.Sequential()
+    model.add(kel.LSTM(num_units, input_shape=(num_timesteps, num_features), return_sequences=True))
+    model.add(kel.TimeDistributed(kel.Dense(num_targets, activation='sigmoid')))
+    model.compile(loss='mse', optimizer='adam')
+    return model
+
+
+def plot_a_single_curve():
+    exp0 = 0.15
+    exp1 = 0.1
+    time, production, stage = calc_two_stage_decline(50., exp0, exp1, 55., 20., num=50)
+    time1, production1, stage1 = calc_two_stage_decline(50., exp0, exp1, 55., 40., num=50)
+    tbapl.plot([(time, production), (time1, production1)], ['p', 'p'])
+    
+
 
 def do_the_thing():
 
-    num_units, RNN = 25, kel.LSTM
-    #num_units, RNN = 75, kel.SimpleRNN
-
     num_features = 2
-    num_timesteps = NUM_PTS
+    num_timesteps = 50
     num_targets = 1
-    num_units = 25
+    num_units = 24
 
-    def make_model():
-        model = kem.Sequential()
-        model.add(RNN(num_units, input_shape=(num_timesteps, num_features), return_sequences=True))
-        model.add(kel.TimeDistributed(kel.Dense(num_targets)))
-        model.compile(loss='mse', optimizer='adam')
-        return model
+    num_stage_change_time = 5
+    num_realizations_per_stage_change_time = 10
+    xdiscspace = (20., 40.)
+    y0_mean = 50.
+    d_stage_zero = 0.15
+    d_stage_one = 0.1
+    xmax = 55.
 
-    num_xdisc = 4
-    num_realizations_per_xdisc = 8
-    xdiscspace = (XDISC_MIN, XDISC_MAX)
-    y0_mean = tbarc.rcparams['shale.exp.y0_mean']
-    d = D
-    k = tbarc.rcparams['shale.exp.k']
-    xmax = XMAX
-
-    num_production_profiles = num_realizations_per_xdisc * num_xdisc    
+    num_production_profiles = num_realizations_per_stage_change_time * num_stage_change_time    
     # for each training sequence we provide it from first datapoint only to the next to last one
     num_sequences_per_profile = num_timesteps - 1
     num_sequences = num_production_profiles * num_sequences_per_profile
@@ -80,15 +94,13 @@ def do_the_thing():
     isample = 0
     production = np.empty((num_timesteps, 1))
     stage = np.empty_like(production)
-    for xdisc in np.linspace(*xdiscspace, num_xdisc):
-        for _ in range(num_realizations_per_xdisc):
+    for xdisc in np.linspace(*xdiscspace, num_stage_change_time):
+        for _ in range(num_realizations_per_stage_change_time):
             y0 = y0_mean
-            pts, ixdisc = tbamk.points_exponential_discontinuous_declinelinear_noisy(y0, d, xmax, xdisc, 
-              num=NUM_PTS)
-            _, q = tbdpt.point_coordinates(pts)  
+            t, q, s = calc_two_stage_decline(y0, d_stage_zero, d_stage_one, xmax, xdisc, 
+              num=num_timesteps) 
             production[:, 0] = q[:]
-            stage[:ixdisc, 0] = 0.0
-            stage[ixdisc:, 0] = 1.0
+            stage[:, 0] = s[:]
             normalizer_production.transform(production)
             normalizer_stage.transform(stage)
             for num_sample_points in range(1, num_timesteps):
@@ -102,35 +114,36 @@ def do_the_thing():
         print(y)
         l()
 
-    if 1:
-        model = make_model()
+    if 0:
+        model = make_rnn(num_features, num_targets, num_timesteps, num_units)
 
         # batch_size should be whole denominator of num sequences per curve
-        model.fit(x, y, epochs=2, batch_size=2)
+        model.fit(x, y, epochs=2, batch_size=5, validation_split=0.2)
 
         model.save(FNAME_MODEL)
     else:
         model = kem.load_model(FNAME_MODEL)
 
 
-    xdisc = xdiscspace[0] + np.diff(xdiscspace)[0] * 0.2
-    pts, ixdisc = tbamk.points_exponential_discontinuous_declinelinear_noisy(y0, d, xmax, xdisc, 
-      num=NUM_PTS)
-    time, production = tbdpt.point_coordinates(pts)
+    xdisc = 20.0
+    time, production, stage = calc_two_stage_decline(y0, d_stage_zero, d_stage_one, xmax, xdisc, 
+      num=num_timesteps)
     num_production_history = 6
     production_in = np.full((num_timesteps, 1), NA)
     production_in[:num_production_history, 0] = production[:num_production_history]
     stage_in = np.zeros_like(production_in)
-    stage_in[ixdisc:, 0] = 1.0
-    
+    stage_in[:, 0] = stage[:]
+
     normalizer_stage.transform(stage_in)
     normalizer_production.transform(production_in)
     xin = np.empty((1, num_timesteps, num_features))
     xin[0, :, ifeature_production] = production_in[:, 0]
     xin[0, :, ifeature_stage] = stage_in[:, 0]
     y_hat = model.predict(xin) 
+
     normalizer_production.inverse_transform(y_hat[0])
-    tbapl.plot([(time[1:], y_hat[0, 1:, 0]), (time[:num_production_history], production[:num_production_history])], ['l', 'p'])
+
+    #tbapl.plot([(time[1:], y_hat[0, 1:, 0]), (time[:num_production_history], production[:num_production_history])], ['l', 'p'])
     tbapl.plot([(time[1:], y_hat[0, 1:, 0]), (time, production)], ['l', 'p'])
     sys.exit()
 
@@ -146,8 +159,6 @@ def do_the_thing():
       secstyles=['lstage'], secylim=(None, 3), secylabel='stage')
 
 
-
-
-
 if __name__ == '__main__':
+    #plot_a_single_curve()
     do_the_thing()
